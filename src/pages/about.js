12e0fe1c4fab31817e8f87b821c8c64f983bc9a6
kahboom <rachelyordan@gmail.com>
2020-09-07 19:07:06 +0100

    add picture css

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,6 +3,7 @@ import { graphql } from 'gatsby';
 import Layout from '../components/Layout';
 import SEO from '../components/SEO';
 import '../styles/elements/marquee.css';
+import '../styles/elements/picture.css';
 
 import Img from "gatsby-image"
 
