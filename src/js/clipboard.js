92a4af673bcea91fab129d3b0aaa5b1bdf99cd48
kahboom <rachelyordan@gmail.com>
2020-09-21 11:56:57 +0100

    rm clipboard for now

---
+++ src/js/clipboard.js
@@ -0,0 +1,17 @@
+// COPY TO CLIPBOARD FUNCTIONALITY
+
+const clipboard = new ClipboardJS('.btn');
+
+clipboard.on('success', function(e) {
+  console.info('Action:', e.action);
+  console.info('Text:', e.text);
+  console.info('Trigger:', e.trigger);
+
+  e.clearSelection();
+});
+
+clipboard.on('error', function(e) {
+  console.error('Action:', e.action);
+  console.error('Trigger:', e.trigger);
+});
+
