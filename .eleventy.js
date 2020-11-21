77221e8de35ab97b950d58328483bc834662e04c
kahboom <rachelyordan@gmail.com>
2020-11-21 14:27:35 +0000

    actually, they're above the fold, so don't

--- .eleventy.js
+++ .eleventy.js
@@ -5,7 +5,7 @@ const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const pluginNavigation = require("@11ty/eleventy-navigation");
 const markdownIt = require("markdown-it");
 const markdownItAnchor = require("markdown-it-anchor");
-const GA_ID = require("./src/_data/metadata.json").googleAnalyticsId;
+//const GA_ID = require("./src/_data/metadata.json").googleAnalyticsId;
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(pluginRss);
